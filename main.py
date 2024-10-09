f723257772c4702f7941cedbc501eea57d77aaee
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-10-09 08:19:39 +0000

    gfbgbh

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 import random
-pole = [1,22,13,45,9,61,37,89,4,]
-print(pole)
+pole1 = [1,22,13,45,9,61,37,89,4,]
+print(pole1)
 
 
-prvni = pole[0]
-prostedni = pole[len(pole) // 2]
-posledni = pole[-1]
+prvni = pole1[0]
+prostedni = pole1[len(pole1) // 2]
+posledni = pole1[-1]
 
 print("první hodnota:", prvni)
 print("prostřední hodnota:", prostedni)
 print("poslední hodnota:", posledni)
 
-pole[5]=34
-print(pole)
+pole1[5]=34
+print(pole1)
 
-pole7 = pole[7]
+pole7 = pole1[7]
 print( pole7)
 
 
-print(len(pole))
+print(len(pole1))
 
 #minimální a maximální hodnota obou pole A
-print(min(pole))
-print(max(pole))
+print(min(pole1))
+print(max(pole1))
 
-pole1 = [6,2,10,15,7,9,11,4,]
-print(pole1)
-
-
-
-#sečtení obou polí a délka obou polí
-print(sum(pole+pole1))
+pole2 = [6,2,10,15,7,9,11,4,]
+print(pole2)
 
 
-soucet = pole[1] + pole[5]
+
+#sečtení obou polí 
+print(sum(pole1+pole2))
+
+
+soucet = pole1[1] + pole1[5]
 print(soucet)
+
+pole_auto=list(range(1,9))
+random.shuffle(pole_auto)
+print(pole_auto)
 
 
 
